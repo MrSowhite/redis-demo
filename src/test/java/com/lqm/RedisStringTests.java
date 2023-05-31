6d1355324f850704f533dad318708e008fe336dc
MrSowhite <751996168@qq.com>
2023-05-31 22:21:09 +0800

    5

--- src/test/java/com/lqm/RedisStringTests.java
+++ src/test/java/com/lqm/RedisStringTests.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @SpringBootTest
 public class RedisStringTests {
 
-    // 4
+    // 5
     // StringRedisTemplate类的key和value的序列化方式默认为String方式，省去了自定义RedisTemplate的过程
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
